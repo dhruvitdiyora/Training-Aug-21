141d174b5044c08fed42afa84ef340e9e2de1504
Dhruvit Diyora <dhruvit.d9688@gmail.com>
2021-08-13 19:36:34 +0530

    REFS: #526 and fixes #526 SQL day 4 tasks

--- SQL/Dhruvit-Diyora/Day1/Assignment.sql
+++ SQL/Dhruvit-Diyora/Day1/Assignment.sql
@@ -1,26 +1,28 @@
-create table inventory(
+--You have been hired to create a relational database to support a car sales business. You need to store information on the business’s employees, inventory, and completed sales. You also need to account for the fact that each salesperson receives a different percentage of their sales in commission. What tables and columns would you create in your relational database, and how would you link the tables?
+
+CREATE TABLE Inventory(
 ProductId INT PRIMARY KEY Identity(1,1),
 ProdName varchar(50) NOT NULL,
 ProdPrice decimal(8,2) NOT NULL,
 );
-create table sales(
+CREATE TABLE Sales(
 SalesId INT PRIMARY KEY Identity(1,1),
-SalesPerson int NOT NULL,
-ProductId int NOT NULL,
-SaleDate date NOT NULL,
-CONSTRAINT FK_SalesPerson FOREIGN KEY (SalesPerson) REFERENCES employee(EmployeeId),
-CONSTRAINT FK_ItemId FOREIGN KEY (ProductId) REFERENCES inventory(ProductId),
+SalesPerson INT NOT NULL,
+ProductId INT NOT NULL,
+SaleDate DATE NOT NULL,
+CONSTRAINT FK_SalesPerson FOREIGN KEY (SalesPerson) REFERENCES Employee(EmployeeId),
+CONSTRAINT FK_ItemId FOREIGN KEY (ProductId) REFERENCES Inventory(ProductId),
 );
-create table employee(
+CREATE TABLE Employee(
 EmployeeId INT PRIMARY KEY Identity(1,1), 
-EmpName varchar(50) NOT NULL,
-EmpLastName varchar(50) NOT NULL,
-EmpType varchar(10) NOT NULL,
-Email varchar(50) CONSTRAINT ckEmail CHECK(Email LIKE '_%@__%.__%'),
-CommisionPer decimal(4,2) NULL
+EmpName VARCHAR(50) NOT NULL,
+EmpLastName VARCHAR(50) NOT NULL,
+EmpType VARCHAR(10) NOT NULL,
+Email VARCHAR(50) CONSTRAINT ckEmail CHECK(Email LIKE '_%@__%.__%'),
+CommisionPer DECIMAL(4,2) NULL
 );
-insert into inventory values ('Audi',835689.00),('TATA',296588.00),('Hyundai',895672.00),('Suzuki',589742.00),('Tesla',85285);
+INSERT INTO Inventory VALUES ('Audi',835689.00),('TATA',296588.00),('Hyundai',895672.00),('Suzuki',589742.00),('Tesla',85285);
 
-insert into employee values ('dhruvit','diyora','Sales',12.20),('leo','messi','Manager',10.00),('sergio','ramos','marketing',2.20),('kylian','mbappe','Sales',12.20);
+insert INTO Employee values ('dhruvit','diyora','Sales',12.20),('leo','messi','Manager',10.00),('sergio','ramos','marketing',2.20),('kylian','mbappe','Sales',12.20);
 
-insert into sales values (2,3),(2,2),(3,2),(2,5);
+insert into Sales values (2,3),(2,2),(3,2),(2,5);
--- SQL/Dhruvit-Diyora/Day2/assignment.sql
+++ SQL/Dhruvit-Diyora/Day2/assignment.sql
@@ -1,103 +1,100 @@
 --update queries
 --task1-Write a SQL statement to change the Email column of Employees table with ‘not available’ for all employees.
 
-alter table Employees DROP CONSTRAINT ukEmail
+ALTER TABLE Employees DROP CONSTRAINT ukEmail
 
-update Employees set Email='not available'
+update Employees SET Email='not available'
 
-alter table Employees ADD CONSTRAINT ukEmail UNIQUE (Email)
 --task2-Write a SQL statement to change the Email and CommissionPct column of employees table with ‘not available’ and 0.10 for all employees.
 
-update Employees set Email='not available', CommissionPct=0.10
+UPDATE Employees SET Email='not available', CommissionPct=0.10
 
 --task3-Write a SQL statement to change the Email and CommissionPct column of employees table with ‘not available’ and 0.10 for those employees whose DepartmentID is 110.
 
-update Employees set Email='not available', CommissionPct=0.10 where DepartmentID=110;
+UPDATE Employees SET Email='not available', CommissionPct=0.10 WHERE DepartmentID=110;
 
 --task4-Write a SQL statement to change the Email column of employees table with ‘not available’ for those employees whose DepartmentID is 80 and gets a commission is less than 20%
 
-update Employees set Email='not available' where DepartmentID=80 AND CommissionPct<0.20
+UPDATE Employees SET Email='not available' WHERE DepartmentID=80 AND CommissionPct<0.20
 
 --task5-Write a SQL statement to change the Email column of employees table with ‘not available’ for those employees who belongs to the ‘Accouning’ department.
 
-update Employees set Email='not available' from Employees E join Departments D on D.DepartmentID=E.DepartmentID where D.DepartmentName='Accounting'
+UPDATE Employees SET Email='not available' FROM Employees E join Departments D ON D.DepartmentID=E.DepartmentID WHERE D.DepartmentName='Accounting'
 
 --task6-Write a SQL statement to change salary of employee to 8000 whose ID is 105, if the existing salary is less than 5000.
 
-update Employees set Salary = CASE WHEN salary<5000 then 8000 ELSE Salary END WHERE EmployeeID=105
+UPDATE Employees SET Salary = CASE WHEN salary<5000 then 8000 ELSE Salary END WHERE EmployeeID=105
 
 --task7-Write a SQL statement to change job ID of employee which ID is 118, to SH_CLERK if the employee belongs to department, which ID is 30 and the existing job ID does not start with SH.
 
-update Employees set JobId='SH_CLERK' where EmployeeID=118 AND DepartmentID=30 AND JobId!='SH%'
+UPDATE Employees SET JobId='SH_CLERK' WHERE EmployeeID=118 AND DepartmentID=30 AND JobId!='SH%'
 
 --task8-Write a SQL statement to increase the salary of employees under the department 40, 90 and 110 according to the company rules that, salary will be increased by 25% for the department 40, 15% for department 90 and 10% for the department 110 and the rest of the departments will remain same.
 
-update Employees 
-	set Salary = 
-		case 
-		when DepartmentID=40 
-			then Salary+((0.25*Salary))
-		when DepartmentID=90
-			then (Salary+(0.15*Salary))
-		when DepartmentID=110
-			then (Salary+(0.10*Salary))
+UPDATE Employees 
+	SET Salary = 
+		CASE 
+		WHEN DepartmentID=40 
+			THEN Salary+((0.25*Salary))
+		WHEN DepartmentID=90
+			THEN (Salary+(0.15*Salary))
+		WHEN DepartmentID=110
+			THEN (Salary+(0.10*Salary))
 		ELSE 
 			Salary
-		end
-
---task9-·         Write a SQL statement to increase the minimum and maximum salary of PU_CLERK by 2000 as well as the salary for those employees by 20% and commission by 10% .
+		END
 
 
 
 --basic select queries
 --task1-Get all employee details from the Employee table
 
-select * from Employees
+SELECT * FROM Employees
 
 --task2-Get FirstName, LastName from Employees table
 
-select FirstName,LastName from Employees
+SELECT FirstName,LastName FROM Employees
 
 --task3-Get FirstName from Employees table using alias name “Employee Name”
 
-select FirstName AS 'Employee Name' from Employees
+SELECT FirstName AS 'Employee Name' FROM Employees
 
 --task4-Get employee details from Employees table whose Employee Name is “Steven”
 
-select * from Employees where FirstName='Steven'
+SELECT * FROM Employees WHERE FirstName='Steven'
 
 --task5-Get employee details from Employees table whose Employee Name are “Neena” and “Lex”
 
-select * from Employees where FirstName='Neena' OR FirstName='Lex'
+SELECT * FROM Employees WHERE FirstName='Neena' OR FirstName='Lex'
 
 --task6-Get employee details from Employees table whose Employee name are not “Neena” and “Neena”
 
-select * from Employees where FirstName!='Neena' OR FirstName!='Nina'
+SELECT * FROM Employees WHERE FirstName!='Neena' OR FirstName!='Nina'
 
 --task7-Get employee details from Employees table whose Salary between 5000 and 8000
 
-select * from Employees where Salary BETWEEN 5000 AND 8000
+SELECT * FROM Employees WHERE Salary BETWEEN 5000 AND 8000
 
 --task8-Write a query to get the names (FirstName, LastName), Salary, PF of all the Employees (PF is calculated as 12% of salary).
 
-select CONCAT(FirstName,' ', LastName) AS names,Salary,(Salary*0.12) AS 'PF' from Employees
+SELECT CONCAT(FirstName,' ', LastName) AS names,Salary,(Salary*0.12) AS 'PF' FROM Employees
 
 --task9-Get employee details from Employees table whose FirstName starts with ‘N’
 
-select * from Employees where FirstName LIKE 'N%'
+SELECT * FROM Employees WHERE FirstName LIKE 'N%'
 
 --task10-Write a query to get unique department ID from Employees table
 
-SELECT DISTINCT DepartmentID FROM employees;
+SELECT DISTINCT DepartmentID FROM Employees;
 
 --task11-Write a query to get unique department ID from Employees table
 
-select * from Employees Order By FirstName DESC
+SELECT * FROM Employees ORDER BY FirstName DESC
 
 --task12-Write a query to get the EmployeeID, names (FirstName, LastName), salary in ascending order of salary.
 
-select EmployeeID,  CONCAT(FirstName,' ', LastName) AS names, Salary from Employees Order By Salary ASC
+SELECT EmployeeID,  CONCAT(FirstName,' ', LastName) AS Names, Salary FROM Employees ORDER BY Salary ASC
 
 --task13-Select TOP 2 salary from employee table
 
-SELECT TOP 2 Salary from Employees 
+SELECT TOP 2 Salary FROM Employees 
