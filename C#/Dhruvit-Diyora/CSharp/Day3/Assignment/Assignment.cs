402775faec4ae13b6e53f46ec956732ddb8aa05c
Dhruvit Diyora <dhruvit.d9688@gmail.com>
2021-11-24 12:47:13 +0530

    REFS #1881 fixes issue #1881: c# Day 3 Practice and assignments Completed

---
+++ C#/Dhruvit-Diyora/CSharp/Day3/Assignment/Assignment.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day3
+{
+    interface IEmp
+    {
+        void Get();
+        void Display();
+        void Salary();
+
+
+    }
+    abstract class Employee : IEmp
+    {
+        static int AccountID;
+        string Name, Address,PanNumber;
+        public virtual void Display()
+        {
+            Console.WriteLine($"Employee's Account Id is {AccountID} , Name is {Name} , Address is {Address} , PAN is {PanNumber}");
+        }
+
+        public virtual void Get()
+        {
+            AccountID += 1;
+            Console.WriteLine("Enter Name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Address:");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter Pan Number:");
+            PanNumber = Console.ReadLine();
+        }
+
+        public virtual void Salary()
+        {
+            //Console.WriteLine("working");
+        }
+    }
+    class PartTime : Employee
+    {
+        int NoOfHour, SalePerHour;
+        public override void Salary()
+        {
+            base.Salary();
+            Console.WriteLine("Enter no of hours worked:");
+            NoOfHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter sales per hr:");
+            SalePerHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your salary should be {NoOfHour*SalePerHour}");
+
+        }
+
+    }
+    class FullTime : Employee
+    {
+        int Basic, TA, HRA, DA;
+        public override void Salary()
+        {
+            base.Salary();
+            Console.WriteLine("Enter Basics");
+            Basic = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter TA");
+            TA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter HRA");
+            HRA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter DA");
+            TA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your salary should be {Basic*TA*HRA*DA}");
+        }
+    }
+    class Assignment
+    {
+        public void functioncall(IEmp obj)
+        {
+            obj.Get();
+            obj.Salary();
+            obj.Display();
+        }
+        static void Main(string[] args)
+        {
+            IEmp obj = null;
+            Console.WriteLine("Select job type:\n1 for Parttime job\n2 for Fulltime job");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    obj = new PartTime();
+                    break;
+                case 2:
+                    obj = new FullTime();
+                    break;
+                default:
+                    Console.WriteLine("invalid input");
+                    break;
+            }
+
+            Assignment assign = new Assignment();
+            assign.functioncall(obj);
+        }
+    }
+}
---
+++ C#/Dhruvit-Diyora/CSharp/Day3/Practice/Interface.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day3
+{
+    interface IBank
+    {
+        void Createaccount();
+        void Closeaccount();
+        void Withdraw();
+        void Deposit();
+        void display();
+
+    }
+    abstract class Bank : IBank
+    {
+        static int AccountID;
+        protected float balance, amountdeposited, initialbalance;
+        string name, address;
+        bool Accountstatus;
+
+        public virtual void Createaccount()
+        {
+            AccountID += 1;
+            Console.WriteLine("Enter Name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter Initial Balance");
+            initialbalance = Convert.ToSingle(Console.ReadLine());
+            Accountstatus = true;
+        }
+        public virtual void Closeaccount()
+        {
+            Accountstatus = false;
+        }
+        public virtual void Deposit()
+        {
+            Console.WriteLine("Enter Amount to be deposited");
+            amountdeposited = Convert.ToSingle(Console.ReadLine());
+            balance += amountdeposited;
+        }
+        public abstract void Withdraw();
+        public virtual void display()
+        {
+            Console.WriteLine($"Account id {AccountID}Name is {name} Address is {address} Balance {balance}");
+        }
+    }
+    class Saving : Bank
+    {
+
+        public override void Withdraw()
+        {
+            Console.WriteLine("Saving WithDraw Function");
+        }
+    }
+    class FixedDeposit : Bank
+    {
+        float tenure, interest, maturityamount;
+        DateTime createdate, maturitydate;
+        public override void Createaccount()
+        {
+            base.Createaccount();
+            Console.WriteLine("Enter Tenure in month");
+            tenure = Convert.ToSingle(Console.ReadLine());
+            createdate = DateTime.Now;
+            Console.WriteLine("Enter Rate of Interest");
+            interest = Convert.ToInt32(Console.ReadLine());
+            maturitydate = createdate.AddMonths((int)tenure);
+            maturityamount = (initialbalance * (interest / 100)) + initialbalance;
+
+        }
+        public override void Withdraw()
+        {
+            if (DateTime.Now >= maturitydate)
+            {
+                balance = maturityamount;
+            }
+            else
+            {
+                throw new Exception($"Time is left Maturity date is {maturitydate} ");
+            }
+        }
+        public void getspecialbenifit()
+        {
+            Console.WriteLine("Credit card issue");
+        }
+    }
+    class Current : Bank
+    {
+        public new void Createaccount()
+        {
+            Console.WriteLine("Create Account current");
+        }
+        public override void Withdraw()
+        {
+            Console.WriteLine("Current Function");
+        }
+    }
+
+    class Interface
+    {
+        public void FuncionInvoke(IBank bank)
+        {
+            if (bank.GetType().Name == "Current")
+            {
+                Current current = (Current)bank;
+                current.Createaccount();
+                bank.Deposit();
+                bank.Withdraw();
+                bank.Closeaccount();
+                bank.display();
+            }
+            else
+            {
+
+                bank.Createaccount();
+                bank.Deposit();
+                bank.Withdraw();
+                bank.Closeaccount();
+                bank.display();
+            }
+            if (bank.GetType().Name == "FixedDeposit")
+            {
+                FixedDeposit fixedDeposit = (FixedDeposit)bank;
+                fixedDeposit.getspecialbenifit();
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            IBank bank = null;
+            Console.WriteLine("Enter 1 for saving Enter 2 for current Enter 3 fixed deposit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    bank = new Saving();
+
+                    break;
+                case 2:
+                    bank = new Current();
+                    break;
+                case 3:
+                    bank = new FixedDeposit();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+
+
+            }
+            var Interface = new Interface();
+            Interface.FuncionInvoke(bank);
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
