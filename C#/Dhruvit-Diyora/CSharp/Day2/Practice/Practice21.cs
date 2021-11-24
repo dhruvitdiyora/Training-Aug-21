dcb8aef46e03409f8a493e020aeec14a65fe0966
Dhruvit Diyora <dhruvit.d9688@gmail.com>
2021-11-24 12:45:56 +0530

    REFS #1880 fixes issue #1880: c# Day 2 Practice and assignments Completed

---
+++ C#/Dhruvit-Diyora/CSharp/Day2/Practice/Practice21.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    class Practice21
+    {
+        class Employee
+        {
+            public int Age { get; set; }
+
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee emp = new Employee();
+                emp.Age = 29;
+                Console.WriteLine("Age is: {0}", emp.Age);
+                emp.Name = "Kanan";
+                Console.WriteLine("Name is: {0}", emp.Name);
+            }
+        }
+    }
+}
